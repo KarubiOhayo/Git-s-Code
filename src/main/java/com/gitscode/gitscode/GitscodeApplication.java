6010e4e2243e1ea6f0cf726169cfdf35c20955d7
김규섭 <pens9053@gmail.com>
2025-09-17 09:47:31 +0900

    spring boot settings

---
+++ src/main/java/com/gitscode/gitscode/GitscodeApplication.java
@@ -0,0 +1,13 @@
+package com.gitscode.gitscode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitscodeApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(GitscodeApplication.class, args);
+  }
+
+}
